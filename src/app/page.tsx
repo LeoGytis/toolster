c32c85cc1ce0f68aa81d8d7de556f3fbb5026500
leogytis <leogytis@gmail.com>
2024-02-12 15:48:54 +0200

    goood styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ const MenuItem = ({ icon, item }: NavLinkProps) => {
 	return (
 		<Link
 			href={href}
-			className="w-40 max-h-32 flex flex-col gap-8 items-center text-[#9acd32] hover:text-[#99cd3286] border radius-sm border-gray-500 shadow-xl ring-4 ring-gray-500 bg-gray-800 px-10 py-4"
+			className="w-40 flex flex-col gap-8 items-center text-[#9acd32] hover:text-[#99cd3286] border radius-sm border-gray-500 rounded-lg shadow-lg hover:shadow-xl bg-gray-800 px-10 py-4"
 		>
 			{React.cloneElement(icon, { className: "h-16 w-20" })}
 			<div className="capitalize whitespace-no-wrap text-center">
