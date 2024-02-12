3d7c8a4375237eabda5a79bd38b1b574379dcec6
leogytis <leogytis@gmail.com>
2024-02-12 20:21:47 +0200

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
+import MenuItem from "@/components/MenuItem";
 import { navLinksData } from "@/components/NavMenu";
 import Image from "next/image";
-import React, { ReactElement } from "react";
-import Link from "next/link";
 
 const Home = () => {
 	return (
@@ -22,27 +21,3 @@ const Home = () => {
 };
 
 export default Home;
-
-interface NavLinkProps {
-	item: string | "";
-	icon: ReactElement;
-}
-
-const MenuItem = ({ icon, item }: NavLinkProps) => {
-	const href = `/${item}`;
-
-	return (
-		<Link
-			href={href}
-			className="w-40 flex flex-col gap-8 items-center text-[#9acd32] hover:text-[#99cd3286] border radius-sm border-gray-500 rounded-lg shadow-lg hover:shadow-xl bg-gray-800 px-10 py-4"
-		>
-			{React.cloneElement(icon, { className: "h-16 w-20" })}
-			<div className="capitalize whitespace-no-wrap text-center">
-				{item
-					.split("-")
-					.map((word) => word.charAt(0) + word.slice(1))
-					.join(" ")}
-			</div>
-		</Link>
-	);
-};
