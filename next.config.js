28e4aadfd9e387b83b648cebca6308b89226cb1b
leogytis <leogytis@gmail.com>
2024-02-16 19:50:10 +0200

    memo for deploying

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	output: 'export',
+	output: "export",
 	images: {
 		unoptimized: true,
 		remotePatterns: [
@@ -12,5 +12,9 @@ const nextConfig = {
 		],
 	},
 };
+
+// To deployt 2024-02-16 comments these two lines in file nextjs.yml
+// # - name: Static HTML export with Next.js  // commented so it would build
+// # run: ${{ steps.detect-package-manager.outputs.runner }} next export
 
 module.exports = nextConfig;
