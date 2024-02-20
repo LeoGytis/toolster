8a4e2e636e077cb553185d1b876c6df70e921e8b
leogytis <leogytis@gmail.com>
2024-02-20 19:48:29 +0200

    image props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,15 @@ const Home = () => {
 	return (
 		<div className="flex flex-col h-screen bg-[#4e5b69]">
 			<div className="relative w-full h-1/6 flex justify-center items-center shadow-xl">
-				<Image src="../images/gears.jpg" alt="home_header" layout="fill" objectFit="cover" className="z-0" />
+				<Image
+					src="../images/gears.jpg"
+					alt="home_header"
+					width={500}
+					height={300}
+					// layout="fill"
+					// objectFit="cover"
+					className="w-full h-full object-cover z-0"
+				/>
 			</div>
 			<div className="h-5/6 items-center p-8">
 				<section className="flex gap-10 items-center flex-wrap">
